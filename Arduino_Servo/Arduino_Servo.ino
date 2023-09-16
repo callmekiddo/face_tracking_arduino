05605d60cdb3984d5c79b0070c60d7b051d7f4ef
callmekiddo <v11071996@gmail.com>
2023-09-17 01:12:36 +0700

    ft

--- Arduino_Servo/Arduino_Servo.ino
+++ Arduino_Servo/Arduino_Servo.ino
@@ -1,6 +1,10 @@
 #include<Servo.h>
-Servo servoTil; //Xoay doc
-Servo servoPan; //Xoay ngang
+Servo servoTilt; // Xoay dọc
+Servo servoPan; // Xoay ngang
+int currentPosX = 90;
+int targetPosX ;
+int currentPosY = 90;
+int targetPosY;
 int x;
 int y;
 int prevX;
@@ -8,34 +12,52 @@ int prevY;
 void setup()
 {
   Serial.begin(9600);
-  servoTil.attach(5); //Doc gan vao 5
-  servoPan.attach(6); //Ngang gan vao 6
-  servoTil.write(90);
+  servoTilt.attach(5); // Dọc gắn vào 5
+  servoPan.attach(6); // Ngang gắn vào 6
+  servoTilt.write(90);
   servoPan.write(90);
 }
 void Pos()
 {
   if(prevX != x || prevY != y)
   {
-    int servoX = map(x, 600, 0, 70, 179);
-    int servoY = map(y, 450, 0, 179, 95);
-    servoX = min(servoX, 179);
-    servoX = max(servoX, 70);
-    servoY = min(servoY, 179);
-    servoY = max(servoY, 95);
+    int servoX = map(x, 300, -300, 180, 0);
+    int servoY = map(y, -300, 300, 180, 0);
+    servoX = min(servoX, 180);
+    servoX = max(servoX, 0);
+    servoY = min(servoY, 180);
+    servoY = max(servoY, 0);
     
-    servoPan.write(servoX);
-    servoTil.write(servoY);
+    targetPosX = servoX;
+    targetPosY = servoY;
+  }
+
+  if (currentPosX != targetPosX) {
+    if (currentPosX < targetPosX) {
+      currentPosX++; // Tăng góc quay lên
+    } else {
+      currentPosX--; // Giảm góc quay xuống
+    }
+    servoPan.write(currentPosX); // Đặt góc quay cho servoPan
+  }
+  if (currentPosY != targetPosY) {
+    if (currentPosY < targetPosY) {
+      currentPosY++; 
+    } else {
+      currentPosY--;
+    }
+    servoTilt.write(currentPosY); // Đặt góc quay cho servoTilt
   }
 }
+
 void loop()
 {
   if(Serial.available() > 0)
   {
-    if(Serial.read() == 'X')
+    if(Serial.read() == 'X') // Nhận ký tự X 
     {
       x = Serial.parseInt();
-      if(Serial.read() == 'Y')
+      if(Serial.read() == 'Y') // Nhận ký tự Y
       {
         y = Serial.parseInt();
        Pos();
